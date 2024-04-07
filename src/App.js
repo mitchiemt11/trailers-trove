da7f2c90f573ba3b6a51c93c12598c10cfb623ba
Mitchell <mitchiemt11@gmail.com>
2024-04-07 14:50:58 +0200

    Add complete header

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       setShowConfetti(false);
       setShowLanding(false);
     }, 4000);
-   }
+  }
 
 
   const handleSearchTrailer = (event) => {
@@ -69,20 +69,29 @@ function App() {
         <Landing handleGetStarted={handleGetStarted} showConfetti={showConfetti} />
       ) : (
         <>
-          <header>
-            <div className='flex justify-between items-center max-w-[1000px] ml-0 mr-0 '>
-              <div className='text-xl'>Trailers Trove</div>
+          <header className="text-white px-8 py-6">
+            <div className='flex justify-between items-center max-w-[1000px] ml-auto mr-auto '>
+              <div className='text-2xl'>Trailers Trove</div>
               <form onSubmit={handleSearchTrailer}>
-                <div className="flex">
+                <label for="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                  </div>
                   <input
                     type="search"
-                    placeholder="Search"
                     value={searchTerm}
                     onChange={(event) => setSearchTerm(event.target.value)}
-                    className="rounded-t-l-lg rounded-b-l-lg border-solid border-[#ccc] p-10 w-full focus:outline-none"
+                    id="search"
+                    className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search"
                   />
-                  <button type="submit" className="bg-[#aeb0b1] border-none px-8 py-12 border-t-r-lg border-b-r-lg text-white cursor-pointer">
-                    <i className="fa fa-search"></i>
+                  <button
+                    type="submit"
+                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Search
                   </button>
                 </div>
               </form>
