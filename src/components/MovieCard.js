abe643044a936d2f9564d8a185ad20d1849764eb
Mitchell <mitchiemt11@gmail.com>
2024-04-10 16:11:02 +0200

    Add a movie title gap

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ const MovieCard = ({ movie, setSelectedMovie }) => {
   return (
     <div className="cursor-pointer" onClick={() => setSelectedMovie(movie)}>
       <img className='w-full' src={movie.thumbnail} alt={movie.title} />
-      <h5 className='font-bold m-0 text-[1.2rem]'>{movie.title}</h5>
+      <h5 className='font-bold m-0 text-[1.2rem] mt-2'>{movie.title}</h5>
     </div>
   );
 };
