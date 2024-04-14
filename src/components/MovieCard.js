0b17244075e7abf583171165f8fd381397e2f78f
Mitchell <mitchiemt11@gmail.com>
2024-04-14 12:00:59 +0200

    Use a better thumbnail for NWOp

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import React from 'react';
 const MovieCard = ({ movie, setSelectedMovie }) => {
   return (
     <div className="cursor-pointer hover:animate-fade-in opacity-80 transition duration-300 ease-in-out hover:opacity-100" onClick={() => setSelectedMovie(movie)}>
-      <img className='w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 hover:scale-110' src={movie.thumbnail} alt={movie.title} />
+      <img className='w-full h-full  scale-100 transform object-cover object-center opacity-100 transition duration-300 hover:scale-110' src={movie.thumbnail} alt={movie.title} />
     </div>
   );
 };
