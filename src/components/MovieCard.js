ecdf7fd72d2e0fa23a5a6078695cb402227ad9fb
Mitchell <mitchiemt11@gmail.com>
2024-04-14 13:03:48 +0200

    Add a rounded border on each card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const MovieCard = ({ movie, setSelectedMovie }) => {
   return (
-    <div className="cursor-pointer hover:animate-fade-in opacity-80 transition duration-300 ease-in-out hover:opacity-100" onClick={() => setSelectedMovie(movie)}>
-      <img className='w-full h-full  scale-100 transform object-cover object-center opacity-100 transition duration-300 hover:scale-110' src={movie.thumbnail} alt={movie.title} />
+    <div className="cursor-pointer  hover:animate-fade-in opacity-80 transition duration-300 ease-in-out hover:opacity-100" onClick={() => setSelectedMovie(movie)}>
+      <img className='w-full h-full rounded-xl scale-100 transform object-cover object-center opacity-100 transition duration-300 hover:scale-110' src={movie.thumbnail} alt={movie.title} />
     </div>
   );
 };
